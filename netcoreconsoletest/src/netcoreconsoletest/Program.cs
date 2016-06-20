7c0a49ea3ef030f2f334e71a4c46a92b8dfbdcb6
petroslikidis <petros.likidis@gmail.com>
2016-06-20 13:24:43 +0200

    Initial commit

---
+++ netcoreconsoletest/src/netcoreconsoletest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace netcoreconsoletest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            string connectionString = @"Data Source=XXX;Initial Catalog=SDB_Metabas;User Id=XXX;Password=XXX;";
+
+            try
+            {
+                using (var conn = new SqlConnection(connectionString))
+                {
+
+                    var tables = conn.Query<MainTableC>("select * from MainTable");
+
+                    foreach (var tbl in tables)
+                    {
+                        Console.WriteLine(tbl.MainTable);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class MainTableC
+    {
+        public string MainTable { get; set; }
+        //public string Status { get; set; }
+        //public string Text { get; set; }
+        public string TableId { get; set; }
+
+    }
+}
